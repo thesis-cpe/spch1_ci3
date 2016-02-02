5ac79cbd512ba4488e279b8b67a30771e5a5c969
thesis-cpe <witthayaniphon@hotmail.com>
2016-02-02 15:46:32 +0700

    เปลี่ยน footer

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
     <!-- To the right -->
     <div class="pull-right hidden-xs">
-      Business Success Created By Kan Accounting Co.,Ltd. (cr. Supachoke Chutimakuntawee).
+        <img  style="height: 39px; padding-bottom: 8px" src="<?php echo base_url();?>uploads/logo_png.jpg"/> <label style="font-size: 23px; color: #eb6d6d">Business Success Created By Kan</label>
     </div>
     <!-- Default to the left -->
     <strong>Copyright &copy; 2015 <a href="<?php echo base_url();?>index.php/main_panel">Kan Accounting Co.,Ltd</a>.</strong> All rights reserved.
